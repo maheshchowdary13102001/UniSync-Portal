f275793b6f3c828426c1377b57ba3e3516e89fb2
Gunnam Mahesh Chowdary <maheshchowdary13102001@gmail.com>
2024-03-19 00:05:19 +0530

    feat(components): header added

--- src/app/Components/ui-header/ui-header.component.ts
+++ src/app/Components/ui-header/ui-header.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-ui-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ui-header.component.html',
   styleUrl: './ui-header.component.scss'
 })
 export class UiHeaderComponent {
-
+  logoPath: string = 'assets/graduation.png';
+  headerList = [
+    {
+      title: 'Home',
+      link: '/'
+    },
+    {
+      title: 'About',
+      link: '/about' 
+    },
+    {
+      title: 'Services',
+      dropdownItems: [
+        { title: 'Service 1', link: '/services/service1' },
+        { title: 'Service 2', link: '/services/service2' },
+        { title: 'Service 3', link: '/services/service3' }
+      ]
+    },
+    {
+      title: 'Contact',
+      link: '/contact'
+    }
+  ]
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { UiHeaderComponent } from './Components/ui-header/ui-header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DashboardComponent],
+  imports: [RouterOutlet,DashboardComponent,UiHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
